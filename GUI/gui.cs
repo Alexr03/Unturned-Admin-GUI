b482804aff3ea088c79deda2e9a40a08e29ebc10
alexr03 <alexredding180@gmail.com>
2016-11-05 19:33:24 +0000

    Changes

--- GUI/gui.cs
+++ GUI/gui.cs
@@ -7,14 +7,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace GUI
 {
     class CommandGUI : PlayerCaller, IRocketCommand
     {
-        bool visible = false;
 
         public string Help
         {
